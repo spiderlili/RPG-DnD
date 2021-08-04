4666e7b2d7798407bafdd0c4efec1da1067cd1cf
Jing Tan <jtan1993@gmail.com>
2021-08-04 16:36:10 +0100

    import ui

---
+++ Assets/Scripts/UI/ShowHideUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InventoryExample.UI
+{
+    public class ShowHideUI : MonoBehaviour
+    {
+        [SerializeField] KeyCode togglePanelKey = KeyCode.Escape;
+        [SerializeField] KeyCode toggleHammerKey = KeyCode.A;
+        [SerializeField] GameObject uiPanelContainer = null;
+        [SerializeField] GameObject uiHammerContainer = null;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            uiPanelContainer.SetActive(false);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(togglePanelKey))
+            {
+                uiPanelContainer.SetActive(!uiPanelContainer.activeSelf);
+            }
+
+            if (Input.GetKeyDown(toggleHammerKey)) {
+                uiHammerContainer.SetActive(!uiHammerContainer.activeSelf);
+            }
+        }
+    }
+}
